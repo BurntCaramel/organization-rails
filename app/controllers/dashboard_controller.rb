d5186eb86103c46462b6f410784df8999c638f55
Patrick Smith <pgwsmith@gmail.com>
2016-09-25 01:23:05 +1000

    Add intro to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,17 @@
 class DashboardController < ApplicationController
+  include ActionView::Helpers::UrlHelper
+
   before_action :check_current_user
 
   def index
     @callback_url = "#{ root_url }auth/auth0/callback"
+
+    @intro = %{
+Royal Icing lets you design and prototype directly from your content.
+
+Content-first design is #{ link_to 'recommended', 'http://alistapart.com/blog/post/content-first-design' } by many leading design firms. [links]
+
+Catalog text, images, records, and user stories, using familiar tools such as #hashtags.
+    }
   end
 end
