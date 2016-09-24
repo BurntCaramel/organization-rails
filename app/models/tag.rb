ebbe45fd755c71bbb0bac7eca6159b41611b00d9
Patrick Smith <pgwsmith@gmail.com>
2016-09-24 23:40:12 +1000

    Ensure tag names are lowercased

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,15 @@ class Tag < ApplicationRecord
   belongs_to :organization
   has_many :item_relationships, class_name: 'ItemTagRelationship', dependent: :destroy
 
+  before_save :downcase_name
+
   validates :organization_id, presence: true
   validates :name, presence: true
+
+  private
+
+    # Ensure name is all-lowercase
+    def downcase_name
+      self.name = name.downcase
+    end
 end
