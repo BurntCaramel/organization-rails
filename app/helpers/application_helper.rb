f54d82c0af2403c2d67b8e564540cc71748a8973
Patrick Smith <pgwsmith@gmail.com>
2016-09-25 21:53:59 +1000

    Add nicer page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,20 @@
 module ApplicationHelper
+  def site_title
+    'Royal Icing'
+  end
+
+  def page_title
+    components = []
+
+    if @organization.present?
+      components << "@#{ @organization.name }"
+    end
+
+    components << site_title
+
+    components.join(' · ')
+  end
+
   def is_record_taken_error?(error, attribute)
     # See http://stackoverflow.com/a/32231499/652615
     error.details.all? do |hash_element|
